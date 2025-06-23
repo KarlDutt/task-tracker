427a568e76e2adb7470c210f70318329ea2d060a
KarlDutt <karlrasmusd@gmail.com>
2025-06-23 19:55:28 +0300

    update_task at least completed works. in progress and to do not

--- tasktracker.py
+++ tasktracker.py
@@ -21,7 +21,7 @@ def add():
         task = {
             "id": str(uuid.uuid1()),
             "description": sys.argv[2],
-            "status": 0,
+            "status": 0, #0=incomplete, 1= in progress, 2= completed
             "createdAt": current_time,
             "updatedAt": current_time
         }
@@ -31,15 +31,35 @@ def add():
 #show all the tasks. only the description and status
 def list_tasks():
     if len(sys.argv) >= 2 and sys.argv[1] == "list":
-        with open(FILENAME, "r", encoding="utf-8") as file:
-            listing_list = json.load(file)
-            for task in listing_list:
+            for task in items_list:
                 print(task["description"])
                 print(task["status"])
 
+#update using description
+#can update task name. can update status. always needs to add updatedAt
+#tasktracker.py update "watch tv" "completed" / to "watch tv later"
+def update_task():
+    if len(sys.argv) >= 4 and sys.argv[1] == "update":
+        updated = False
+        for task in items_list:
+            if task["description"] == sys.argv[2]:
+                updated = False
+                if sys.argv[3] == "completed":
+                    task["status"] = 2
+                    updated = True
+                elif sys.argv[3] == "in progress":
+                    task["status"] = 1
+                    updated = True
+                elif sys.argv[3] == "to":
+                        task["description"] = sys.argv[4]
+                        updated = True
+                if updated:
+                     task["updatedAt"] = datetime.now().isoformat()
+                     print("task updated") 
 
 add()
 list_tasks()
+update_task()
 #save new list to json
 with open("biglist.json", mode="w", encoding="utf-8") as write_file:
     json.dump(items_list, write_file, indent=4)

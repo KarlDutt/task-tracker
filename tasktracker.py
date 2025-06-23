ffa8013dd41b567e15ef1d027aac56d3c68fd16a
KarlDutt <karlrasmusd@gmail.com>
2025-06-23 15:20:29 +0300

    adds a dictionary

--- tasktracker.py
+++ tasktracker.py
@@ -4,11 +4,13 @@ import sys
 import json
 import os
 import uuid
+from datetime import datetime
 
 FILENAME = "biglist.json"
+current_time = datetime.now().isoformat()
 
 #read json or create new if there's none
-if os.path.exists(FILENAME):
+if os.path.exists(FILENAME) and os.path.getsize(FILENAME) > 0:
     with open(FILENAME, "r", encoding="utf-8") as file:
         items_list = json.load(file)
 else:
@@ -17,11 +19,18 @@ else:
 #add an item to list
 def add():
     if len(sys.argv) >= 2 and sys.argv[1] == "add":
-        items_list.append(sys.argv[2])
+
+        task = {
+            "id": str(uuid.uuid1()),
+            "description": sys.argv[2],
+            "status": 0,
+            "createdAt": current_time,
+            "updatedAt": current_time
+        }
+        items_list.append(task)
 
 add()
 
 #save new list to json
 with open("biglist.json", mode="w", encoding="utf-8") as write_file:
-    json.dump(items_list, write_file)
-
+    json.dump(items_list, write_file, indent=4)
